bd1aabfd945594a63724f65b357bec952123b416
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-09-25 16:17:25 +0900

    仮確定

--- Material.cpp
+++ Material.cpp
@@ -6,10 +6,10 @@ void Material::Initilize()
 {
 	cBuffer_->Map(0, nullptr, reinterpret_cast<void**>(&constMap_));
 	
-	constMap_.color					= color_;
-	constMap_.enableLighting		= static_cast<uint32_t>(enableLighting_);
-	constMap_.shiness				= shiness_;
-	constMap_.uvTransform			= MakeIdentity4x4();
+	constMap_->color				= color_;
+	constMap_->enableLighting		= static_cast<uint32_t>(enableLighting_);
+	constMap_->shiness				= shiness_;
+	constMap_->uvTransform			= MakeIdentity4x4();
 }
 
 void Material::LoadTexture(const std::string& _dirPath)
