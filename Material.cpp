6126bb95badb83bd633208a7231894dce62aa40a
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-09-25 16:16:28 +0900

    Material仮

--- Material.cpp
+++ Material.cpp
@@ -1,6 +1,18 @@
 #include "Material.h"
+#include "MatrixFunction.h"
+
 
 void Material::Initilize()
 {
+	cBuffer_->Map(0, nullptr, reinterpret_cast<void**>(&constMap_));
+	
+	constMap_.color					= color_;
+	constMap_.enableLighting		= static_cast<uint32_t>(enableLighting_);
+	constMap_.shiness				= shiness_;
+	constMap_.uvTransform			= MakeIdentity4x4();
+}
 
+void Material::LoadTexture(const std::string& _dirPath)
+{
+	textureFileName_ = _dirPath;
 }
--- Material.h
+++ Material.h
@@ -9,6 +9,7 @@
 
 #include <d3d12.h>
 #include <wrl.h>
+#include <string>
 
 class Material
 {
@@ -22,18 +23,13 @@ class Material
 	};
 
 public:
-	// カラー
-	Vector4					color_						= { 1.0f,1.0f ,1.0f ,1.0f };
-	// ライティングの有無
-	bool					enableLighting_				= true;
-	// 光沢度
-	float					shiness_					= 40.0f;
-	// スケール
-	Vector2					scale_						= { 1.0f ,1.0f };
-	// 回転
-	float					rotate_						= 0.0f;
-	// 移動
-	Vector2					transform_					= { 0.0f ,0.0f };
+	Vector4					color_						= { 1.0f,1.0f ,1.0f ,1.0f };	// カラー
+	bool					enableLighting_				= true;							// ライティングの有無
+	float					shiness_					= 40.0f;						// 光沢度
+	Vector2					scale_						= { 1.0f ,1.0f };				// スケール
+	float					rotate_						= 0.0f;							// 回転	
+	Vector2					transform_					= { 0.0f ,0.0f };				// 移動
+	std::string				textureFileName_			= {};							// テクスチャファイル名
 
 	Material() = default;
 	~Material() = default;
@@ -44,16 +40,30 @@ public:
 	void Initilize();
 
 	/// <summary>
+	/// テクスチャの読み込み 
+	/// </summary>
+	/// <param name="_dirPath"></param>
+	void LoadTexture(const std::string& _dirPath);
+
+	/// <summary>
 	/// 定数バッファの取得
 	/// </summary>
 	/// <returns></returns>
 	const Microsoft::WRL::ComPtr<ID3D12Resource>& GetConstBuffer() const { return cBuffer_; }
 
+	/// <summary>
+	/// テクスチャハンドルの取得
+	/// </summary>
+	/// <returns></returns>
+	const int32_t GetTextureHandle() const { return textureHandle_; }
+
 private:
 	// 定数バッファ
 	Microsoft::WRL::ComPtr<ID3D12Resource> cBuffer_;
 	// Map用変数
-	CBufferDate constMap_;
+	CBufferDate* constMap_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_;
 	// コピー禁止
 	Material(const Material&) = delete;
 	Material operator=(const Material&) = delete;
