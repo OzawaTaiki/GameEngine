935392fde66fc9c9d06b89be9925cc862f8d86cd
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-01-09 13:14:11 +0900

    hlslライトの計算を関数に

--- Sample/Resources/Shader/Object3d.PS.hlsl
+++ Sample/Resources/Shader/Object3d.PS.hlsl
@@ -19,38 +19,53 @@ cbuffer gColor : register(b2)
     float4 materialColor;
 }
 
+struct DirectionalLight
+{
+    float4 color;
+    float3 direction;
+    float intensity;
+    int isHalf;
+};
+
+struct PointLight
+{
+    float4 color;
+    float3 position;
+    float intensity;
+    float radius;
+    float decay;
+    int isHalf;
+};
+
+struct SpotLight
+{
+    float4 color;
+    float3 position;
+    float intensity;
+    float3 direction;
+    float distance;
+    float decay;
+    float cosAngle;
+    float cosFalloutStart;
+    int isHalf;
+};
+
 //平行光源
 cbuffer gDirectionalLight : register(b3)
 {
-    float4 DL_color; //ライトの色
-    float3 DL_direction; //ライトの向き
-    float DL_intensity; //輝度
-    int DL_isHalf;
+    DirectionalLight DL;
 }
 
 //点光源
 cbuffer gPointLight : register(b4)
 {
-    float4 PL_color;
-    float3 PL_position;
-    float PL_intensity;
-    float PL_radius;
-    float PL_decay;
-    int PL_isHalf;
+    PointLight PL;
 }
 
 //スポットライト
 cbuffer gSpotLight : register(b5)
 {
-    float4 SL_color;
-    float3 SL_position;
-    float SL_intensity;
-    float3 SL_direction;
-    float SL_ditance;
-    float SL_decay;
-    float SL_cosAngle;
-    float SL_cosFalloutStart;
-    int SL_isHalf;
+    SpotLight SL;
 }
 
 struct PixelShaderOutput
@@ -61,6 +76,8 @@ struct PixelShaderOutput
 Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
+float3 CalculateDirectionalLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor);
+float3 CalculatePointLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor);
 float3 CalculateSpotLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor);
 
 PixelShaderOutput main(VertexShaderOutput _input)
@@ -69,72 +86,18 @@ PixelShaderOutput main(VertexShaderOutput _input)
     output.color = float4(0.0f, 0.0f, 0.0f, 0.0f);
     float4 textureColor;
 
-    //画像の有無
-    //if (isVisible == 1.0f)
-    //{
-        float4 transformedUV = mul(float4(_input.texcoord, 0.0f, 1.0f), unTransform);
-        textureColor = materialColor * gTexture.Sample(gSampler, transformedUV.xy);
-    //}
-    //else
-    //    textureColor = materialColor;
+    float4 transformedUV = mul(float4(_input.texcoord, 0.0f, 1.0f), unTransform);
+    textureColor = materialColor * gTexture.Sample(gSampler, transformedUV.xy);
 
-    /*lmbart*/
     float3 toEye = normalize(worldPosition - _input.worldPosition);
 
-    float3 diffuse = float3(0.0f, 0.0f, 0.0f);
-    float3 specular = float3(0.0f, 0.0f, 0.0f);
-    if (DL_intensity > 0.0f)
-    {
-        float cos;
-        float NdotL = dot(normalize(_input.normal), -DL_direction);
-        if (DL_isHalf != 0)
-        {
-            cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        }
-        else
-        {
-            cos = saturate(NdotL);
-        }
-
-        float3 halfVector = normalize(-DL_direction + toEye);
-        float NDotH = dot(normalize(_input.normal), halfVector);
-        float specularPow = pow(saturate(NDotH), shininess);
-
-        diffuse = materialColor.rgb * textureColor.rgb * DL_color.rgb * cos * DL_intensity;
-        specular = DL_color.rgb * DL_intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
-
-
-    }
-
-// Point Lightの計算（PL_intensity > 0 の場合のみ）
-    float3 PLDiffuse = float3(0.0f, 0.0f, 0.0f);
-    float3 PLSpecular = float3(0.0f, 0.0f, 0.0f);
-    if (PL_intensity > 0.0f)
-    {
-        float3 PLdirection = normalize(_input.worldPosition - PL_position);
-        float3 PLHalfVector = normalize(-PLdirection + toEye);
-        float PLspecularPow = pow(saturate(dot(normalize(_input.normal), PLHalfVector)), shininess);
-
-        float PL_NdotL = dot(normalize(_input.normal), -PLdirection);
-        float PL_cos = saturate(PL_NdotL);
-        if (PL_isHalf != 0)
-        {
-            PL_cos = pow(PL_NdotL * 0.5f + 0.5f, 2.0f);
-        }
-
-        float distance = length(PL_position - _input.worldPosition);
-        float factor = pow(saturate(-distance / PL_radius + 1.0f), PL_decay);
-
-        PLDiffuse = materialColor.rgb * textureColor.rgb * PL_color.rgb * PL_cos * PL_intensity * factor;
-        PLSpecular = PL_color.rgb * PL_intensity * PLspecularPow * float3(1.0f, 1.0f, 1.0f) * factor;
-
-    }
-
+    float3 directionalLight = CalculateDirectionalLighting(_input, toEye, textureColor);
+    float3 pointLight = CalculatePointLighting(_input, toEye, textureColor);
     float3 spotLightcColor = CalculateSpotLighting(_input, toEye, textureColor);
 
     if (enableLighting != 0)
     {
-        output.color.rgb = diffuse + specular + PLDiffuse + PLSpecular + spotLightcColor;
+        output.color.rgb = directionalLight + pointLight + spotLightcColor;
         output.color.a = materialColor.a * textureColor.a;
     }
     else
@@ -149,37 +112,73 @@ PixelShaderOutput main(VertexShaderOutput _input)
     return output;
 }
 
+float3 CalculateDirectionalLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor)
+{
+    if (DL.intensity <= 0.0f)
+        return float3(0.0f, 0.0f, 0.0f);
+    float3 HalfVector = normalize(-DL.direction + _toEye);
+    float specularPow = pow(saturate(dot(normalize(_input.normal), HalfVector)), shininess);
+    float NdotL = dot(normalize(_input.normal), -DL.direction);
+    float cos = saturate(NdotL);
+    if (DL.isHalf != 0)
+    {
+        cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+    }
+    float3 diffuse = materialColor.rgb * _textureColor.rgb * DL.color.rgb * cos * DL.intensity;
+    float3 specular = DL.color.rgb * DL.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
+    return diffuse + specular;
+}
+
+float3 CalculatePointLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor)
+{
+    if (PL.intensity <= 0.0f)
+        return float3(0.0f, 0.0f, 0.0f);
+    float3 direction = normalize(_input.worldPosition - PL.position);
+    float3 HalfVector = normalize(-direction + _toEye);
+    float specularPow = pow(saturate(dot(normalize(_input.normal), HalfVector)), shininess);
+    float NdotL = dot(normalize(_input.normal), -direction);
+    float cos = saturate(NdotL);
+    if (PL.isHalf != 0)
+    {
+        cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+    }
+    float distance = length(PL.position - _input.worldPosition);
+    float factor = pow(saturate(-distance / PL.radius + 1.0f), PL.decay);
+    float3 diffuse = materialColor.rgb * _textureColor.rgb * PL.color.rgb * cos * PL.intensity * factor;
+    float3 specular = PL.color.rgb * PL.intensity * specularPow * float3(1.0f, 1.0f, 1.0f) * factor;
+    return diffuse + specular;
+}
 
 float3 CalculateSpotLighting(VertexShaderOutput _input, float3 _toEye, float4 _textureColor)
 {
-    if (SL_intensity <= 0.0f)
+    if (SL.intensity <= 0.0f)
         return float3(0.0f, 0.0f, 0.0f);
 
 
-    float3 direction = normalize(_input.worldPosition - SL_position);
+    float3 direction = normalize(_input.worldPosition - SL.position);
     float3 HalfVector = normalize(-direction + _toEye);
     float specularPow = pow(saturate(dot(normalize(_input.normal), HalfVector)), shininess);
 
     float NdotL = dot(normalize(_input.normal), -direction);
     float cos = saturate(NdotL);
-    if (SL_isHalf != 0)
+    if (SL.isHalf != 0)
     {
         cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
     }
 
-    float distance = length(SL_position - _input.worldPosition);
-    float factor = pow(saturate(-distance / SL_ditance + 1.0f), SL_decay);
+    float distance = length(SL.position - _input.worldPosition);
+    float factor = pow(saturate(-distance / SL.distance + 1.0f), SL.decay);
 
-    float cosAngle = dot(direction,normalize (SL_direction));
+    float cosAngle = dot(direction, normalize(SL.direction));
     float falloffFactor = 1.0f;
-    if (cosAngle < SL_cosFalloutStart)
+    if (cosAngle < SL.cosFalloutStart)
     {
-        falloffFactor = saturate((cosAngle - SL_cosAngle) / (SL_cosFalloutStart - SL_cosAngle));
+        falloffFactor = saturate((cosAngle - SL.cosAngle) / (SL.cosFalloutStart - SL.cosAngle));
     }
 
 
-    float3 diffuse = materialColor.rgb * _textureColor.rgb * SL_color.rgb * cos * SL_intensity * factor*falloffFactor;
-    float3 specular = SL_color.rgb * SL_intensity * specularPow * float3(1.0f, 1.0f, 1.0f) * factor * falloffFactor;
+    float3 diffuse = materialColor.rgb * _textureColor.rgb * SL.color.rgb * cos * SL.intensity * factor * falloffFactor;
+    float3 specular = SL.color.rgb * SL.intensity * specularPow * float3(1.0f, 1.0f, 1.0f) * factor * falloffFactor;
 
     return diffuse + specular;
 
