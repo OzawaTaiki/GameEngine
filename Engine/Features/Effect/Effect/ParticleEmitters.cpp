6fe8a31213fac0bf2ceb0a8ee7585a819e52436a
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-01-24 17:51:45 +0900

    a

--- Engine/Features/Effect/Effect/ParticleEmitters.cpp
+++ Engine/Features/Effect/Effect/ParticleEmitters.cpp
@@ -134,8 +134,6 @@ void ParticleEmitter::Draw() const
     case EmitterShape::Circle:
         // TODO : 円の描画
         {
-        Matrix4x4 affine = MakeAffineMatrix({ radius_ }, rotate_, position_);
-        LineDrawer::GetInstance()->DrawCircle(affine);
         }
         break;
     case EmitterShape::None:
