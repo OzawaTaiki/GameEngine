c5e326d11ee6bb79c2910be4343afe8f942b7985
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-03-31 09:53:46 +0900

    JSONバインダーの初期化とレイヤー管理の改善
    `CollisionLayerManager`に`JsonBinder`を追加し、JSONデータの初期化と保存を行うメソッド`InitJsonBinder`を実装しました。レイヤー名を格納する`layerNames_`ベクターを追加し、レイヤー取得時にこのベクターにレイヤー名を追加する処理を追加しました。ヘッダーファイルでは、これらのメンバー変数とメソッドの宣言を行い、デストラクタをカスタムに変更しました。

--- Engine/Features/Collision/CollisionLayer/CollisionLayerManager.h
+++ Engine/Features/Collision/CollisionLayer/CollisionLayerManager.h
@@ -3,7 +3,10 @@
 #include <string>
 #include <unordered_map>
 #include <cstdint>
+#include <vector>
+#include <memory>
 
+class JsonBinder;
 class CollisionLayerManager
 {
 public:
@@ -12,14 +15,20 @@ public:
     // Layerを取得する
     uint32_t GetLayer(const std::string& _layer);
 
+    void InitJsonBinder();
+
 private:
 
     // Layerのマップ
     std::unordered_map<std::string, uint32_t> layerMap_;
+    std::vector<std::string> layerNames_;
+
+    std::unique_ptr<JsonBinder> jsonBinder_;
+
 
 private:
     CollisionLayerManager();
-    ~CollisionLayerManager() = default;
+    ~CollisionLayerManager();
     CollisionLayerManager(const CollisionLayerManager&) = delete;
     CollisionLayerManager& operator=(const CollisionLayerManager&) = delete;
 
