229dc716c1ecd96654217633832d3550869a73e3
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-05-29 21:34:18 +0900

    PSOFlagsの処理を追加
    `ParticleSystem::AddParticle` および `ParticleSystem::AddParticles` メソッドにおいて、PSOFlagsを取得し、特定のフラグを追加する処理を実装しました。これにより、未登録のPSOFlagsが新たに登録され、パーティクルシステムのパフォーマンスが向上します。また、各グループに対してpsoIndexが設定されるようになりました。

--- Engine/Features/Effect/Manager/ParticleSystem.cpp
+++ Engine/Features/Effect/Manager/ParticleSystem.cpp
@@ -192,6 +192,15 @@ void ParticleSystem::AddParticle(const std::string& _groupName, const std::strin
     }
     else
     {
+        PSOFlags psoFlags = _settings.GetPSOFlags();
+        psoFlags |= PSOFlags::Type_Particle | PSOFlags::Depth_mZero_fLEqual;
+
+        // PSOFlagsが未登録の場合は登録する
+        if (!psoMap_.contains(psoFlags))
+            psoMap_[psoFlags] = PSOManager::GetInstance()->GetPipeLineStateObject(psoFlags).value();
+
+        it->second.psoIndex = psoFlags;
+
         it->second.particles.push_back(std::move(particle_ptr));
     }
 
@@ -253,6 +262,15 @@ void ParticleSystem::AddParticles(const std::string& _groupName, const std::stri
             group.particles.push_back(std::unique_ptr<Particle>(particle));
         }
         group.textureHandle = _textureHandle;
+
+        PSOFlags psoFlags = _settings.GetPSOFlags();
+        psoFlags |= PSOFlags::Type_Particle | PSOFlags::Depth_mZero_fLEqual;
+
+        // PSOFlagsが未登録の場合は登録する
+        if (!psoMap_.contains(psoFlags))
+            psoMap_[psoFlags] = PSOManager::GetInstance()->GetPipeLineStateObject(psoFlags).value();
+
+        group.psoIndex = psoFlags;
 
         // モデルが異なる場合のみ更新
         if (group.model == nullptr || group.key.modelName != _useModelName)
