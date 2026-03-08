44a90cca55bbe3058b0cb6f0e56b117b8aa4a9f4
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2026-03-09 04:43:43 +0900

    VST3モジュールの追加
    - 新しいソースファイル `VST3Module.cpp` と `VST3Module.h` を追加し、VST3プラグインのロードと管理機能を実装
    - プロジェクトファイル `GameEngine.vcxproj` に新しいファイルを追加
    - プロジェクトフィルタ `GameEngine.vcxproj.filters` に新しいファイルを適切に分類
    
    推奨ブランチ名 : feature/vst3_module

---
+++ Engine/System/Audio/VST3/VST3Module.cpp
@@ -0,0 +1,50 @@
+#include "VST3Module.h"
+#include "VST3Plugin.h"
+
+bool Engine::VST3Module::Load(const std::string& path)
+{
+    std::string errorMessage;
+    // 指定パスのDLLからモジュールを作成する
+    module_ = VST3::Hosting::Module::create(path, errorMessage);
+    if (!module_)
+        return false;
+
+    return true;
+}
+
+std::vector<VST3::Hosting::ClassInfo> Engine::VST3Module::GetAudioEffectClasses() const
+{
+    std::vector<VST3::Hosting::ClassInfo> audioEffectClasses;
+
+    if (!module_)
+        return audioEffectClasses;
+
+    auto factory = module_->getFactory();
+    // DLLの中にあるプラグインのクラス情報をすべて取得する
+    for (auto& classInfo : factory.classInfos())
+    {
+        // クラス情報のカテゴリがエフェクト系プラグインのものであれば、audioEffectClassesに追加する
+        if (classInfo.category() == kVstAudioEffectClass)
+        {
+            audioEffectClasses.push_back(classInfo);
+        }
+        // kVstComponentControllerClass もある
+        // これはGUIを提供するクラス。プラグイン独自のGUIを提供する場合にはこれも必要になる。
+    }
+
+    return audioEffectClasses;
+}
+
+std::unique_ptr<Engine::VST3Plugin> Engine::VST3Module::CreatePlugin(const VST3::Hosting::ClassInfo& classInfo)
+{
+    if (!module_)
+        return nullptr;
+
+    auto plugin = std::make_unique<VST3Plugin>();
+    // ClassInfoからVST3Pluginを生成する
+    bool success = plugin->Load(module_->getFactory(), classInfo);
+    if (!success)
+        return nullptr;
+
+    return plugin;
+}
